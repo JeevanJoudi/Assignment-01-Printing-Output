6b37a6fc499f4610c3f5f5ef54a4f47a8095d3cd
JeevanJoudi <146952539+JeevanJoudi@users.noreply.github.com>
2024-05-16 17:55:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let a =3;
+let b= 5;
+let c;
+let expressions = 
+`
+let a =3;
+let b= 5;
+let c; 
+----------
+a + b = ${a+b} 
+a - b = ${a-b}
+a * b = ${a*b}
+a / b = ${a/b}
+a % b = ${a%b}
+a += b = ${a+=b}
+a -= b = ${a-=b} 
+a *= b = ${a*=b}
+a /= b = ${a/=b}
+a %= b = ${a%=b}
+a == b = ${a==b}
+a != b = ${a!=b}
+a > b = ${a>b}
+a < b = ${a<b}
+!a && !c  = ${!a && !c}
+!a || !c  = ${!a || !c }
+`;
+console.log(expressions);
+
+
+
+let first_name = "Jeevan";
+let last_name = "Joudi";
+let email = "joud@gmail.com";
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+console.log(output);
